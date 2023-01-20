52ff048bfb6dc245de220fe7ad8df4780a2af284
Moonwalker231102 <108191264+Moonwalker231102@users.noreply.github.com>
2023-01-20 09:41:47 -0500

    JS M4 homework finished

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -167,7 +167,7 @@ function esPrimo(num) {
    if (num === 2) return true;
    for (let i = 2; i <= num - 1; i++) { 
       if (num % i === 0) return false;
-  } return true;
+   } return true;
 }
 
 function esVerdadero(valor) {
